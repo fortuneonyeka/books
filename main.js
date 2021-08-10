357de7ec10e7f3bda963cf1e087c92d6e50465c1
ixboy <ismaelixboy@gmail.com>
2021-08-10 21:43:55 +0100

    create UI class and add methods

--- main.js
+++ main.js
@@ -6,52 +6,60 @@ const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 const btn = document.querySelector('.btn');
 const bookHolder = document.querySelector('#bookContainer');
+
 let books = [];
+
 function clearOut() {
   title.value = '';
   author.value = '';
 }
 
 function Book(title, author) {
-  this.title = title;
-  this.author = author;
+    this.title = title;
+    this.author = author;
 }
 
-function getBooks() {
-  books = JSON.parse(localStorage.books);
-  bookHolder.innerHTML = '';
-  let id = 0;
-  for (const book of books) {
-    bookHolder.innerHTML += `
-    <div>
-      <p>${book.title}</p>
-      <p>${book.author}</p>
-      <button onClick="destroyBook(${id++})">Remove</button> 
-    </div>`;
+class UI{
+  
+  static getBooks() {
+    books = JSON.parse(localStorage.books);
+    bookHolder.innerHTML = '';
+    let id = 0;
+    for (const book of books) {
+      bookHolder.innerHTML += `
+      <div>
+        <p><strong>title:</strong> ${book.title}</p>
+        <p><strong>Author:</strong> ${book.author}</p>
+        <button onClick="UI.destroyBook(${id++})">Remove</button> 
+      </div>`;
+    }
   }
+  
+  static updateLocalStorage() {
+    localStorage.books = JSON.stringify(books);
+    UI.getBooks();
+  }
+  
+  static addBook() {
+    const newBook = new Book(title.value, author.value);
+  
+    books.push(newBook);
+  
+    UI.updateLocalStorage();
+  }
+  
+  static destroyBook(id) {
+    books.splice(id, 1);
+    UI.updateLocalStorage();
+  }
+  
 }
 
-function updateLocalStorage() {
-  localStorage.books = JSON.stringify(books);
-  getBooks();
-}
 
-function addBook() {
-  const newBook = new Book(title.value, author.value);
-
-  books.push(newBook);
-
-  updateLocalStorage();
-}
-
-function destroyBook(id) {
-  books.splice(id, 1);
-  updateLocalStorage();
-}
 
 btn.onclick = (e) => {
   e.preventDefault();
-  addBook();
+  UI.addBook();
   clearOut();
 };
-if (localStorage.length > 0) getBooks();
+if (localStorage.length > 0) UI.getBooks();
