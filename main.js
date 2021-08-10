289d356248c42a920180e566382339f141a5c600
ixboy <ismaelixboy@gmail.com>
2021-08-10 17:23:16 +0100

    correct linters

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
-const title = document.querySelector('#title')
-const author = document.querySelector('#author')
-const btn = document.querySelector('.btn')
-const bookHolder = document.querySelector('#bookContainer')
-let books = []
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-plusplus */
+
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const btn = document.querySelector('.btn');
+const bookHolder = document.querySelector('#bookContainer');
+let books = [];
 function clearOut() {
   title.value = '';
   author.value = '';
 }
 
 function Book(title, author) {
-  this.title = title
-  this.author = author
+  this.title = title;
+  this.author = author;
+}
+
+function getBooks() {
+  books = JSON.parse(localStorage.books);
+  bookHolder.innerHTML = '';
+  let id = 0;
+  for (const book of books) {
+    bookHolder.innerHTML += `
+    <div>
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <button onClick="destroyBook(${id++})">Remove</button> 
+    </div>`;
+  }
 }
 
 function updateLocalStorage() {
@@ -19,38 +37,21 @@ function updateLocalStorage() {
 }
 
 function addBook() {
-  const newBook = new Book(title.value, author.value)
+  const newBook = new Book(title.value, author.value);
 
-  books.push(newBook)
-  
-updateLocalStorage();
+  books.push(newBook);
+
+  updateLocalStorage();
 }
-
 
 function destroyBook(id) {
   books.splice(id, 1);
   updateLocalStorage();
 }
 
-
-function getBooks() {
-  books = JSON.parse(localStorage.books);
-  bookHolder.innerHTML = "";
-  let id = 0;
-  for (book of books) {
-    bookHolder.innerHTML += `
-    <div>
-      <p>${book.title}</p>
-      <p>${book.author}</p>
-      <button onClick="destroyBook(${id++})">Remove</button>
-    </div>`;
-  }
-}
-
-
 btn.onclick = (e) => {
   e.preventDefault();
-  addBook()
-  clearOut()
+  addBook();
+  clearOut();
 };
 if (localStorage.length > 0) getBooks();
